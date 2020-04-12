fc86976e7c071b0d13fa032f1e272491a8b4f0cb
George Papadopoulos <virusfree21@gmail.com>
2020-04-12 16:19:21 +0300

    Release v0.0.238

--- 10_AspHosting/Services/Foompany.Services.AspHostingSample/ServiceMain.cs
+++ 10_AspHosting/Services/Foompany.Services.AspHostingSample/ServiceMain.cs
@@ -1,11 +1,6 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Phoesion.Glow.SDK.Firefly.AspHost;
-/*
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
-*/
 
 namespace Foompany.AspHostingSample
 {
@@ -18,17 +13,6 @@ namespace Foompany.AspHostingSample
                           {
                               builder.AddConsole();
                           });
-            /*
-                      .UseSerilog((context, configuration) =>
-                      {
-                          configuration
-                                .MinimumLevel.Debug()
-                                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                                .MinimumLevel.Override("System", LogEventLevel.Warning)
-                                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                                .Enrich.FromLogContext()
-                                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
-                      });*/
         }
     }
 }
