0ec2724a1e6714fdcb6e5ce85db3b294a699fc51
George Papadopoulos <virusfree21@gmail.com>
2020-04-21 16:37:53 +0300

    Release v0.0.243

--- 0a_Service_Custom_Configurations/Foompany.Services.HelloWorld/Modules/Sample.cs
+++ 0a_Service_Custom_Configurations/Foompany.Services.HelloWorld/Modules/Sample.cs
@@ -8,7 +8,7 @@ namespace Foompany.Services.HelloWorld.Modules
     public class Sample : Phoesion.Glow.SDK.Firefly.FireflyModule
     {
         [Autowire]
-        new ServiceMain Service;
+        private new ServiceMain Service;
 
         //----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -18,7 +18,9 @@ namespace Foompany.Services.HelloWorld.Modules
             return $"SampleData1 = {Service.SampleData1} \r\n\r\n" +
                    $"DataWithDefaultValue = {Service.DataWithDefaultValue} \r\n\r\n" +
                    $"SampleNumberConfig = {Service.SampleNumberConfig} \r\n\r\n" +
-                   $"ComplexModelConfig = {Phoesion.Json.JsonConvert.SerializeObject(Service.ComplexModelConfig)} \r\n\r\n";
+                   $"ComplexModelConfig = {ToJson(Service.ComplexModelConfig)} \r\n\r\n" +
+                   $"ValueFromAppSettings = {Service.ValueFromAppSettings} \r\n\r\n" +
+                   $"Configs_MyKey2 = {Service.Configs_MyKey2} \r\n\r\n";
         }
 
         //----------------------------------------------------------------------------------------------------------------------------------------------
--- 0a_Service_Custom_Configurations/Foompany.Services.HelloWorld/ServiceMain.cs
+++ 0a_Service_Custom_Configurations/Foompany.Services.HelloWorld/ServiceMain.cs
@@ -3,6 +3,7 @@
 namespace Foompany.Services.HelloWorld
 {
     /// <summary> The 'main' class of the service. Used for configurations and service setup </summary>
+    [ServiceName("HelloWorld")]
     public class ServiceMain : FireflyService
     {
         [Configuration]
@@ -20,5 +21,12 @@ namespace Foompany.Services.HelloWorld
             Value1 = "v1",
             Value2 = "v2",
         };
+
+        [Configuration]
+        public string ValueFromAppSettings;
+
+        [Configuration("MyKey2")]   //get a specific section from appsettings (not using member name)
+        public string Configs_MyKey2;
+
     }
 }
