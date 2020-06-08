35cb96676dff353905ab2a779786b0632a87126b
George Papadopoulos <virusfree21@gmail.com>
2020-06-08 18:57:47 +0300

    Release v0.0.263

---
+++ 16_BackgroundWorker/Services/Foompany.Services.SampleService1/Modules/DefaultModule.cs
@@ -0,0 +1,23 @@
+﻿using Phoesion.Glow.SDK;
+using Phoesion.Glow.SDK.Firefly;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Foompany.Services.SampleService1.Modules
+{
+    public class DefaultModule : Phoesion.Glow.SDK.Firefly.FireflyModule
+    {
+        [Autowire]
+        MyWorker worker;
+
+        //----------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Action(Methods.GET)]
+        public string Default() =>
+            $"SampleServices is up and running! \r\n" +
+            $"The current count from worker is {worker.Counter}";
+
+        //----------------------------------------------------------------------------------------------------------------------------------------------
+    }
+}
---
+++ 16_BackgroundWorker/Services/Foompany.Services.SampleService1/ServiceMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Phoesion.Glow.SDK;
+using Phoesion.Glow.SDK.Firefly;
+
+
+namespace Foompany.Services.SampleService1
+{
+    [ServiceName("SampleService1")]
+    public class ServiceMain : FireflyService
+    {
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            //Add Worker
+            //Note: you can use the AddHostedService<T>() extension here, but then the 'MyWorker' instance cannot be injected to other classes
+            services.AddSingletonHostedService<MyWorker>();
+        }
+
+        protected override void Configure(IGlowApplicationBuilder app)
+        {
+            //TODO: Configure middleware..
+        }
+    }
+}
